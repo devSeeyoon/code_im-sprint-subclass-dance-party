c0083a3fc26d02d68595847d2c8caf6ad19c057c
hyunsung <hslgood@gmail.com>
2019-11-22 11:59:38 +0900

    change by hs

--- src/pseudoclassical/BlinkyDancer.js
+++ src/pseudoclassical/BlinkyDancer.js
@@ -4,10 +4,25 @@ if (typeof window === 'undefined') {
 
 // blinkyDancer를 pseudoclassical한 방식으로 리팩토링하세요
 // 참고로, constructor는 대문자로 이름을 시작하는 것이 관례입니다
-function BlinkyDancer () {
+function BlinkyDancer (/*top, left, timeBetweenSteps*/) {
   // your code here
-
+  Dancer.call(this);
+  
 }
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+let oldStep = new BlinkyDancer().step;
+
+BlinkyDancer.prototype.step = function () {
+  oldStep();
+
+  let style = this.$node.style;
+  
+  style.display = style.display === 'none' ? 'inline-block' : 'none'; 
+}
+
 
 // you don't have to worry about this code. this is for testing.
 if (typeof window === 'undefined') {
--- src/pseudoclassical/Dancer.js
+++ src/pseudoclassical/Dancer.js
@@ -6,10 +6,33 @@ if (typeof window === 'undefined') {
 
 // blinkyDancer를 pseudoclassical한 방식으로 리팩토링하세요
 // 참고로, constructor는 대문자로 이름을 시작하는 것이 관례입니다
-function Dancer () {
+function Dancer (top, left /*timeBetweenSteps*/) {
   // your code here
-
+  this.$node = createDancerElement();
+  this.step();
+  this.setPostion(top, left)
 }
+
+//메소드 생성
+
+Dancer.prototype.step = function() {
+  setTimeout(this.step, this.timeBetweenSteps)
+}
+
+Dancer.prototype.setPostion = function(top, left) {
+  Object.assign(this.$node.style, {
+    top: `${top}px`,
+    left: `${left}px`
+  });
+}
+
+
+
+const createDancerElement = () => {
+  let elDancer = document.createElement('span');
+  elDancer.className = 'dancer';
+  return elDancer;
+};
 
 // you don't have to worry about this code. this is for testing.
 if (typeof window === 'undefined') {
