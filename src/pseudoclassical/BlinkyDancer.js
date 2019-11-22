97cb8e890812f737d784d296016e161345fec4de
LEEJAEHYUB <woguqdl20@gmail.com>
2019-11-22 13:21:50 +0900

    ES6변환까지 완료 by.JH

--- src/pseudoclassical/BlinkyDancer.js
+++ src/pseudoclassical/BlinkyDancer.js
@@ -1,30 +1,26 @@
 if (typeof window === 'undefined') {
-  var Dancer = require('./Dancer');
+	var Dancer = require('./Dancer');
 } // you don't have to worry about this code. this is for testing.
 
 // blinkyDancer를 pseudoclassical한 방식으로 리팩토링하세요
 // 참고로, constructor는 대문자로 이름을 시작하는 것이 관례입니다
-function BlinkyDancer (/*top, left, timeBetweenSteps*/) {
-  // your code here
-  Dancer.call(this);
-  
+function BlinkyDancer(/*top, left, timeBetweenSteps*/) {
+	// your code here
+	Dancer.call(this);
 }
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-let oldStep = new BlinkyDancer().step;
+BlinkyDancer.prototype.step = function() {
+	// 상속(setTimeout)받은 함수 + style 적용
+	Dancer.prototype.step.call(this);
 
-BlinkyDancer.prototype.step = function () {
-  oldStep();
-
-  let style = this.$node.style;
-  
-  style.display = style.display === 'none' ? 'inline-block' : 'none'; 
-}
-
+	let style = this.$node.style;
+	style.display = style.display === 'none' ? 'inline-block' : 'none';
+};
 
 // you don't have to worry about this code. this is for testing.
 if (typeof window === 'undefined') {
-  module.exports = BlinkyDancer;
+	module.exports = BlinkyDancer;
 }
--- src/pseudoclassical/Dancer.js
+++ src/pseudoclassical/Dancer.js
@@ -1,40 +1,38 @@
 if (typeof window === 'undefined') {
-  var jsdom = require('jsdom');
-  var { JSDOM } = jsdom;
-  var { document } = (new JSDOM('')).window;
+	var jsdom = require('jsdom');
+	var { JSDOM } = jsdom;
+	var { document } = new JSDOM('').window;
 } // you don't have to worry about this code. this is for testing.
 
 // blinkyDancer를 pseudoclassical한 방식으로 리팩토링하세요
 // 참고로, constructor는 대문자로 이름을 시작하는 것이 관례입니다
-function Dancer (top, left /*timeBetweenSteps*/) {
-  // your code here
-  this.$node = createDancerElement();
-  this.step();
-  this.setPostion(top, left)
+function Dancer(top, left, timeBetweenSteps) {
+	// your code here
+	this.$node = createDancerElement();
+	this.timeBetweenSteps = timeBetweenSteps;
+	this.step();
+	this.setPostion(top, left);
 }
 
 //메소드 생성
-
 Dancer.prototype.step = function() {
-  setTimeout(this.step, this.timeBetweenSteps)
-}
+	setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
 
 Dancer.prototype.setPostion = function(top, left) {
-  Object.assign(this.$node.style, {
-    top: `${top}px`,
-    left: `${left}px`
-  });
-}
-
-
+	Object.assign(this.$node.style, {
+		top: `${top}px`,
+		left: `${left}px`,
+	});
+};
 
 const createDancerElement = () => {
-  let elDancer = document.createElement('span');
-  elDancer.className = 'dancer';
-  return elDancer;
+	let elDancer = document.createElement('span');
+	elDancer.className = 'dancer';
+	return elDancer;
 };
 
 // you don't have to worry about this code. this is for testing.
 if (typeof window === 'undefined') {
-  module.exports = Dancer;
+	module.exports = Dancer;
 }
